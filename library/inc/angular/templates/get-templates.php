6fb5e437e1f6729b46fdf68d818cdc717fdf4ba1
royrogers1972@hotmail.com <royrogers1972@hotmail.com>
2014-02-20 01:37:41 +0100

    added comments

--- library/inc/angular/templates/get-templates.php
+++ library/inc/angular/templates/get-templates.php
@@ -6,6 +6,10 @@
  * Time: 15:23
  */
 
+/**
+ * This code generates the angularjs html views templates
+ * It uses HTTP-API with  wp_remote_get()
+ * */
 function angp_get_template_content($new_status, $old_status) {
 	global $post;
 
