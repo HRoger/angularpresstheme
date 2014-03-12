66bef5abcd9822409463ace5016964782642ad81
royrogers1972@hotmail.com <royrogers1972@hotmail.com>
2014-03-12 02:46:46 +0100

    ended searchbar not-found page 404

---
+++ library/scripts/controllers/ctNotFound.js
@@ -0,0 +1,16 @@
+/**
+ * Created by ROGER on 11.03.14.
+ */
+'use strict';
+angularpressApp.controller("NotFoundCtrl", function ($scope, $compile, $location) {
+	//Search input widget
+	$scope.$watch($compile(angular.element("#searchform", function () {
+		$scope.submit = function () {
+			if ($scope.text) {
+				$location.path("/search/").search({'s': $scope.text});
+
+			}
+		}
+
+	}))($scope));
+});
